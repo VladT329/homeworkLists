418134ebe059e4c346641d8e539f27cc864398de
Vladislav <VladT329@gmail.com>
2024-07-27 22:27:43 +0300

    start coding

---
+++ src/main/java/com/list/homeworkLists/HomeworkListsApplication.java
@@ -0,0 +1,13 @@
+package com.list.homeworkLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/list/homeworkLists/HomeworkListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.homeworkLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
